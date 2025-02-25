f5008df5cef45a4d35069659fc622512479734a9
gaukhar26 <gaukhar26.zhanel@mail.ru>
2025-02-25 14:59:39 +0500

    ChildProfile

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.Fragments.ChildProfileFragment
 import com.example.myapplication.Fragments.RequestsListFragment
 
 class MainActivity : AppCompatActivity() {
