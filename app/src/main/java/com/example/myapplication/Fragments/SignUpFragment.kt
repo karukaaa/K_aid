44c53bfbba9fa0421f68973cfeebf7446cfee04d
karukaaa <arukhanym.zhaidary@gmail.com>
2025-03-18 22:55:04 +0500

    added sign up page and improved home page

--- app/src/main/java/com/example/myapplication/Fragments/SignUpFragment.kt
+++ app/src/main/java/com/example/myapplication/Fragments/SignUpFragment.kt
@@ -1,55 +1,52 @@
 package com.example.myapplication.Fragments
 
-import android.health.connect.datatypes.units.Length
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentLogInBinding
+import com.example.myapplication.databinding.FragmentSignUpBinding
 
-class LogInFragment : Fragment() {
+class SignUpFragment : Fragment() {
 
-    private var _binding: FragmentLogInBinding? = null
+    private var _binding: FragmentSignUpBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentLogInBinding.inflate(inflater, container, false)
+        _binding = FragmentSignUpBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     companion object {
-
-        fun newInstance(param1: String, param2: String) = LogInFragment()
+        fun newInstance(param1: String, param2: String) = SignUpFragment()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.loginButton.setOnClickListener{
-            Toast.makeText(requireContext(), "Login button clicked!", Toast.LENGTH_SHORT).show()
+        binding.nextButton.setOnClickListener{
+            Toast.makeText(requireContext(), "Next button clicked!", Toast.LENGTH_SHORT).show()
         }
 
-        binding.signUp.setOnClickListener{
-            val newFragment = SignUpFragment()
+        binding.login.setOnClickListener {
+            val newFragment = LogInFragment()
             parentFragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, newFragment)
                 .commit()
         }
 
-        binding.forgotYourPassword.setOnClickListener{
-            Toast.makeText(requireContext(), "Forgot Password Clicked!", Toast.LENGTH_SHORT).show()
+        binding.uploadPhoto.setOnClickListener {
+            Toast.makeText(requireContext(), "Upload photo button clicked!", Toast.LENGTH_SHORT).show()
         }
-
     }
 }
