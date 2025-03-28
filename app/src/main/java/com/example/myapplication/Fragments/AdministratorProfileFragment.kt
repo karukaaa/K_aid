97f7b33fa051cd41ff0c2d69456f12736a89e5e5
Arukhanym.Zhaidary <arukhanym.zhaidary@MacBook-Pro-BTSD.local>
2025-03-28 12:32:53 +0500

    fdfsdf

--- app/src/main/java/com/example/myapplication/Fragments/AdministratorProfileFragment.kt
+++ app/src/main/java/com/example/myapplication/Fragments/AdministratorProfileFragment.kt
@@ -6,9 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentAdministratorProfileBinding
+import com.example.myapplication.databinding.FragmentLogInBinding
+import com.example.myapplication.databinding.FragmentRequestCreationBinding
 
 class AdministratorProfileFragment : Fragment() {
 
+    private var _binding: FragmentAdministratorProfileBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,10 +23,22 @@ class AdministratorProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_administrator_profile, container, false)
+        _binding = FragmentAdministratorProfileBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     companion object {
         fun newInstance(param1: String, param2: String) = AdministratorProfileFragment()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnCreateRequest.setOnClickListener{
+            val newFragment = RequestCreationFragment()
+            parentFragmentManager.beginTransaction()
+                .add(R.id.fragment_container, newFragment)
+                .commit()
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/Fragments/RequestCreationFragment.kt
+++ app/src/main/java/com/example/myapplication/Fragments/RequestCreationFragment.kt
@@ -6,8 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentLogInBinding
+import com.example.myapplication.databinding.FragmentRequestCreationBinding
 
 class RequestCreationFragment : Fragment() {
+
+    private var _binding: FragmentRequestCreationBinding? = null
+    private val binding get() = _binding!!
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,8 +23,8 @@ class RequestCreationFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_request_creation, container, false)
+        _binding = FragmentRequestCreationBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     companion object {
