4cecda62a41c8f6668ede64fb0336d6ca69548a8
karukaaa <arukhanym.zhaidary@gmail.com>
2025-04-05 01:35:21 +0500

    added children sampled data

---
+++ app/src/main/java/com/example/myapplication/Child.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication
+
+data class Child(
+    val id: Int,
+    val name: String,
+    val orphanageName: String,
+    val age: Int,
+    val description: String,
+    val requests: MutableList<Request> = mutableListOf()
+) {
+    override fun toString(): String {
+        return name
+    }
+}
