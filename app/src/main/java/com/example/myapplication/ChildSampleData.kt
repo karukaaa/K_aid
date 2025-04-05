3e5a16da2b32615a73eafe24bf08c173d4b3279f
karukaaa <arukhanym.zhaidary@gmail.com>
2025-04-05 21:54:17 +0500

    added bds

--- app/src/main/java/com/example/myapplication/ChildSampleData.kt
+++ app/src/main/java/com/example/myapplication/ChildSampleData.kt
@@ -11,7 +11,8 @@ object ChildSampleData {
                 orphanageName = "Sunshine Home",
                 age = 10,
                 description = "A cheerful and energetic girl who loves running.",
-                requests = mutableListOf(RequestSampleData.sampleRequests[0])
+                requests = mutableListOf(RequestSampleData.sampleRequests[0]),
+                dateOfBirthday = "06.08.2018"
             ),
             Child(
                 id = 2,
@@ -19,7 +20,8 @@ object ChildSampleData {
                 orphanageName = "Hope Shelter",
                 age = 11,
                 description = "A quiet boy who enjoys reading and drawing.",
-                requests = mutableListOf(RequestSampleData.sampleRequests[1])
+                requests = mutableListOf(RequestSampleData.sampleRequests[1]),
+                dateOfBirthday = "21.02.2017"
             ),
             Child(
                 id = 3,
@@ -27,7 +29,8 @@ object ChildSampleData {
                 orphanageName = "Rainbow Center",
                 age = 9,
                 description = "Always curious and eager to learn new things.",
-                requests = mutableListOf(RequestSampleData.sampleRequests[2])
+                requests = mutableListOf(RequestSampleData.sampleRequests[2]),
+                dateOfBirthday = "09.12.2020"
             ),
             Child(
                 id = 4,
@@ -35,7 +38,8 @@ object ChildSampleData {
                 orphanageName = "Sunshine Home",
                 age = 8,
                 description = "Loves to celebrate birthdays and share sweets.",
-                requests = mutableListOf(RequestSampleData.sampleRequests[3])
+                requests = mutableListOf(RequestSampleData.sampleRequests[3]),
+                dateOfBirthday = "28.04.2014"
             ),
             Child(
                 id = 5,
@@ -43,7 +47,8 @@ object ChildSampleData {
                 orphanageName = "Hope Shelter",
                 age = 12,
                 description = "Enjoys solving puzzles and reading books.",
-                requests = mutableListOf(RequestSampleData.sampleRequests[4])
+                requests = mutableListOf(RequestSampleData.sampleRequests[4]),
+                dateOfBirthday = "17.11.2013"
             ),
             Child(
                 id = 6,
@@ -51,7 +56,8 @@ object ChildSampleData {
                 orphanageName = "Rainbow Center",
                 age = 7,
                 description = "Loves flying toys and outdoor games.",
-                requests = mutableListOf(RequestSampleData.sampleRequests[5])
+                requests = mutableListOf(RequestSampleData.sampleRequests[5]),
+                dateOfBirthday = "05.09.2015"
             ),
             Child(
                 id = 7,
@@ -59,7 +65,8 @@ object ChildSampleData {
                 orphanageName = "Sunshine Home",
                 age = 6,
                 description = "Has a collection of dolls and loves tea parties.",
-                requests = mutableListOf(RequestSampleData.sampleRequests[6])
+                requests = mutableListOf(RequestSampleData.sampleRequests[6]),
+                dateOfBirthday = "14.07.2016"
             )
         )
     }
